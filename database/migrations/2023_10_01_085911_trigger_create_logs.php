cac6c2e5a746aabf691d9aeacc1ff9bccb398c50
rukaachan <novops911@gmail.com>
2023-10-11 14:01:14 +0700

    update:trigger

--- database/migrations/2023_10_01_085911_trigger_create_logs.php
+++ database/migrations/2023_10_01_085911_trigger_create_logs.php
@@ -21,11 +21,15 @@ return new class extends Migration
     BEGIN
         DECLARE surat_id INT;
         DECLARE userid VARCHAR(200);
+        DECLARE jenissurat VARCHAR(200);
+        DECLARE tanggalsurat DATE;
 
         SELECT username INTO userid FROM tbl_user WHERE id_user = NEW.id_user;
+        SELECT jenis_surat INTO jenissurat FROM jenis_surat WHERE id_jenis_surat = NEW.id_jenis_surat;
+        SELECT tanggal_surat INTO tanggalsurat FROM surat WHERE id_surat = NEW.id_surat;
 
         SELECT id_surat INTO surat_id FROM surat WHERE id_surat = NEW.id_surat;
-        INSERT INTO logs (logs) VALUES (CONCAT(userid, ": Melakukan Penambahan Surat Dengan Nomor ", surat_id));
+        INSERT INTO logs (logs) VALUES (CONCAT(userid, ": Melakukan Penambahan Surat Dengan Nomor ", surat_id, ", Jenis Surat ", jenissurat, " dan Tanggal Surat ", tanggalsurat));
     END'
         );
     }
